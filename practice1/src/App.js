122b26d4f9122b0b6aa5fecab94b841f1f7bf3c6
Alanondoto <alexgusevtoo@gmail.com>
2022-09-18 13:18:08 +0300

    export/import

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -1,4 +1,6 @@
- import './App.css';
+import './App.css';
+import Technologies from './Technologies';
+import Header  from './Header';
 
 const App = () => {
   return (
@@ -6,29 +8,6 @@ const App = () => {
       <Header />
       <Technologies />
     </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href='#s'>Home</a>
-      <a href='#s'>News Feed</a>
-      <a href='#s'>Messages</a>
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div>
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li> 
-          <li>react</li>
-        </ul>
-      </div>
   );
 }
 
