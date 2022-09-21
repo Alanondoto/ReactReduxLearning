b3ae5e9718d364dca73dbddbc0bf3f3605a99a2f
Alanondoto <alexgusevtoo@gmail.com>
2022-09-21 18:51:17 +0300

    auto-import

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Technologies from './Technologies';
 import Header  from './Header';
+import Footer from './footer';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Footer />
       <Technologies />
     </div>
   );
