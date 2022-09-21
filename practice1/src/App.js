90febee9d788072c877fc4bb0e26e39fb6a0f133
Alanondoto <alexgusevtoo@gmail.com>
2022-09-21 23:38:19 +0300

    social-web grid

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -1,14 +1,50 @@
 import './App.css';
-import Technologies from './Technologies';
-import Header  from './Header';
-import Footer from './footer';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Footer />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://cdn.logo.com/hotlink-ok/logo-social.png' alt='logo' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a href='#s'>Profile</a>
+        </div>
+        <div>
+          <a href='#s'>Messages</a>
+        </div>
+        <div>
+          <a href='#s'>News</a>
+        </div>
+        <div>
+          <a href='#s'>Music</a>
+        </div>
+        <div>
+          <a href='#s'>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+        <img src='https://html5css.ru/css/img_lights.jpg' alt='background' />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post2
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
