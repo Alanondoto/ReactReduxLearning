ca83d4c0c857b72729b02cfffc07bc6cb32f247d
Alanondoto <alexgusevtoo@gmail.com>
2022-09-22 12:29:47 +0300

    improving css1.1

---
+++ practice1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,22 @@
+import s from './MyPosts.module.css';
+
+const MyPosts = () => {
+  return (
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div className={s.post}>
+        <div className={s.item}>
+          post 1
+        </div>
+        <div className={s.item}>
+          post 2
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MyPosts;
