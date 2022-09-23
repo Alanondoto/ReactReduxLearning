ea7d5ac8e78303f2e0c0a524e276ef71fc4c31cf
Alanondoto <alexgusevtoo@gmail.com>
2022-09-23 17:12:59 +0300

    props

--- practice1/src/components/Profile/MyPosts/MyPosts.jsx
+++ practice1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,7 +10,8 @@ const MyPosts = () => {
         <button>Add post</button>
       </div>
       <div className={s.post}>
-        <Post />
+        <Post likeCounter='15' message='Hi, how are you?' />
+        <Post likeCounter='20' message='It is my first post' />
       </div>
     </div>
   )
--- practice1/src/components/Profile/MyPosts/Post/Post.jsx
+++ practice1/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
         <div className={s.item}>
           <img src='https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3' alt='avatar'/>
-          post 1
+          {props.message}
           <div>
-          <span>like</span>
+          <span>{props.likeCounter} like</span>
           </div>
         </div>
   )
