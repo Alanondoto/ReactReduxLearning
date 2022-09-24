db35a480dfa30a8a7442215ec50b781677452238
Alanondoto <alexgusevtoo@gmail.com>
2022-09-24 08:37:34 +0300

    Dialogs css

--- practice1/src/components/Dialogs/Dialogs.jsx
+++ practice1/src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Alex
+        </div>
+        <div className={s.dialog}>
+          Masha
+        </div>
+        <div className={s.dialog}>
+          Ignat
+        </div>
+        <div className={s.dialog}>
+          Gora
+        </div>
+        <div className={s.dialog}>
+          Edik
+        </div>
+        <div className={s.dialog}>
+          Billy
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are you?</div>
+        <div className={s.message}>Yo</div>
+      </div>
     </div>
   )
 }
