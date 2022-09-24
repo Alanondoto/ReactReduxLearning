cbd2a46fb43a2cdc4272235d11904295e75edd44
Alanondoto <alexgusevtoo@gmail.com>
2022-09-24 08:00:44 +0300

    NavLink

--- practice1/src/components/Navbar/Navbar.jsx
+++ practice1/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,33 @@
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
         <div className={s.item}>
-          <a href='/profile'>Profile</a>
+          <NavLink to='/profile' className={({ isActive }) =>
+            isActive ? s.active : undefined
+          }>Profile</NavLink>
         </div>
         <div className={s.item}>
-          <a href='/dialogs'>Messages</a>
+          <NavLink to='/dialogs' className={({ isActive }) =>
+            isActive ? s.active : undefined
+          }>Messages</NavLink>
         </div>
         <div className={s.item}>
-          <a href='/news'>News</a>
+          <NavLink to='/news' className={({ isActive }) =>
+            isActive ? s.active : undefined
+          }>News</NavLink>
         </div>
         <div className={s.item}>
-          <a href='/music'>Music</a>
+          <NavLink to='/music' className={({ isActive }) =>
+            isActive ? s.active : undefined
+          }>Music</NavLink>
         </div>
         <div className={s.item}>
-          <a href='/settings'>Settings</a>
+          <NavLink to='/settings' className={({ isActive }) =>
+            isActive ? s.active : undefined
+          }>Settings</NavLink>
         </div>
       </nav>
     )
