adafcdf2157769106f7a6506b78e4e7a46bc4a4e
Alanondoto <alexgusevtoo@gmail.com>
2022-10-09 20:52:59 +0300

    connect, mapStateToProps, mapDispatchToProps

--- practice1/src/components/Dialogs/DialogsContainer.jsx
+++ practice1/src/components/Dialogs/DialogsContainer.jsx
@@ -1,34 +1,25 @@
-// import s from './Dialogs.module.css';
-// import DialogItem from "./DialogItem/DialogItem";
-// import Message from "./Message/Message";
-import React from "react";
+// import React from "react";
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogsReducer";
 import Dialogs from "./Dialogs";
-import StoreContext from "../../storeContext";
 import {connect} from "react-redux";
 
-const DialogsContainer = () => {
+let mapStateToProps = (state) => {
+  return {
+    dialogsPage: state.dialogsPage
+  }
+}
 
-  return <StoreContext.Consumer>
-    { store => {
-      let onSendMessageClick = () => {
-        store.dispatch(sendMessageCreator());
-      }
-      let onNewMessageChange = (body) => {
-        store.dispatch(updateNewMessageBodyCreator(body));
-      }
-      return  <Dialogs updateNewMessageBody={onNewMessageChange}
-                       sendMessage={onSendMessageClick}
-                       dialogsPage={store.getState().dialogsPage}/>
-      }
+let mapDispatchToProps = (dispatch) => {
+  return {
+    updateNewMessageBody: () => {
+      dispatch(sendMessageCreator());
+    },
+    sendMessage: (body) => {
+      dispatch(updateNewMessageBodyCreator(body));
     }
-  </StoreContext.Consumer>
+  }
 }
 
-let f1 = () => {
+const SuperDialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
 
-}
-
-const SuperDialogsContainer = connect()(Dialogs);
-
-export default DialogsContainer;
+export default SuperDialogsContainer;
