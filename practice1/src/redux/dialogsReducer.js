4487bfed57d0eac42de1c33a97e3a574384eb951
Alanondoto <alexgusevtoo@gmail.com>
2022-10-10 22:47:35 +0300

    stateCopy DialogReducer

--- practice1/src/redux/dialogsReducer.js
+++ practice1/src/redux/dialogsReducer.js
@@ -21,20 +21,20 @@ let initialState = {
 };
 
 const dialogsReducer = (state = initialState, action) => {
+
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY: {
-      let stateCopy = {...state};
-      stateCopy.newMessageBody = action.body;
-      return stateCopy;
-    }
-    case SEND_MESSAGE: {
-      let stateCopy = {...state};
-      stateCopy.messages = [...state.messages];
+    case UPDATE_NEW_MESSAGE_BODY:
+      return  {
+        ...state,
+        newMessageBody: action.body
+      };
+    case SEND_MESSAGE:
       let body = state.newMessageBody;
-      stateCopy.messages.push({id: 6, message: body, align: 'right'});
-      stateCopy.newMessageBody = '';
-      return stateCopy;
-    }
+      return  {
+        ...state,
+        newMessageBody: '',
+        messages: [...state.messages, {id: 6, message: body, align: 'right'}]
+      };
     default:
       return state;
   }
