af0ea1866dcf34d8dd1545db2db9d9ef1cf266e6
Alanondoto <alexgusevtoo@gmail.com>
2022-10-17 17:20:11 +0300

    withRouter, props.match.params

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -16,7 +16,9 @@ const App = () => {
         <Navbar />
         <div className='app-wrapper-content'>
           <Routes>
-            <Route path='/profile' element={<ProfileContainer />}/>
+            <Route path="/profile" element={<ProfileContainer/>}>
+              <Route path=":userId" element={<ProfileContainer/>}/>
+            </Route>
             <Route path='/dialogs/*' element={<DialogsContainer />}/>
             <Route path='/news' element={<News />}/>
             <Route path='/music' element={<Music />}/>
